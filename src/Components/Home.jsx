5b656f9afa29dae7c37d92cd6f1054742767b173
Juan Cruz Vincenti <juancruzvincenti@gmail.com>
2020-08-19 18:41:49 -0300

    Creada la home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,77 +1,144 @@
-import React, {useState, useEffect, useRef} from 'react';
-import axios from 'axios';
-import { Grid } from '@material-ui/core';
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
-import Anio from './Anio';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import Carrera from './Carrera';
 
-export default function Home() {
+const drawerWidth = 240;
 
-    const nombreUniversidad = useRef('');
-    const nombreCarrera = useRef('');
-    const anios = useRef('');
-    const [render, setRender] = useState(false);
-    const [materiasAprobadas, setMateriasAprobadas] = useState([]);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    })
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
 
-    function eliminarMateria(materia) {
-        setMateriasAprobadas(materiasAprobadas.filter(materiaAprobada => materiaAprobada !== materia));
-    }
+export default function MiniDrawer() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
 
-    function agregarMateria(materia) {
-        setMateriasAprobadas(materiasAprobadas.concat([materia]));
-    }
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
 
-    useEffect(() => {
-        const getData = () => {
-            if (!render) {
-                axios.get('https://materiasunq2.free.beeceptor.com/tpi')
-                    .then((response) => {
-                        nombreUniversidad.current = response.data.Universidad;
-                        nombreCarrera.current = response.data.Carrera;
-                        anios.current = response.data.Anios;
-                        setRender(true);
-                    })
-            }
-        }
-        getData();
-    }, [render])
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
-    function renderMaterias() {
-        //TODO: Eliminar el object entries
-        return Object.entries(anios.current).map((anio) => (
-            <Anio 
-                key={anio[1].Anio}
-                anio ={anio[1].Anio}
-                materias = {anio[1].Materias}
-                materiasAprobadas = {materiasAprobadas}
-                agregarMateria = {agregarMateria}
-                eliminarMateria = {eliminarMateria}
-            />
-        ));
-    }
-
-    return (
-        <div>
-            <Grid container direction="row" justify="center">
-                <Grid container direction="row"  justify="center">
-                    <Grid item xs={8}>
-                        <Typography variant="h5">
-                            Establecimiento: {nombreUniversidad.current}
-                        </Typography>
-                    </Grid>
-                </Grid>
-                <Grid container direction="row" justify="center">
-                    <Grid item xs={8}>
-                        <Typography variant="h5">
-                            Carrera: {nombreCarrera.current}
-                        </Typography>
-                    </Grid>
-                </Grid>
-                <Grid container direction="row" justify="center">
-                    <Grid item xs={8}>
-                        {renderMaterias()}
-                    </Grid>
-                </Grid>
-            </Grid>
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+        style={{background: 'linear-gradient(45deg, rgba(119,0,0,1) 0%, rgb(185,28,0) 100%)'}}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Seguimiento de materias
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
         </div>
-    );
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+            <Carrera/>
+        </main>
+    </div>
+  );
 }

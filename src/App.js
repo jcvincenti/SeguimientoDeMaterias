e39f18582758dd41a7f846c62e1f516ca69612f3
Juan Cruz Vincenti <juancruzvincenti@gmail.com>
2020-08-20 23:08:10 -0300

    Agregado react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import Home from './Components/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Carrera from './Components/Carrera/Carrera'
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/carrera" component={Carrera}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }

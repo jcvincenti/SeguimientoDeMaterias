4020d809d48dd3a7f37b79f878ff74c5442b8a68
Juan Cruz Vincenti <juancruzvincenti@gmail.com>
2021-04-03 16:13:35 -0300

    Add axios to home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -15,6 +15,7 @@ import NavigationBar from './NavigationBar';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
   icon: {
@@ -31,22 +32,41 @@ const useStyles = makeStyles((theme) => ({
 export default function Home() {
     const classes = useStyles();
     const [universidad, setUniversidad] = useState(null);
+    const [universidades, setUniversidades] = useState(null);
     const [carrera, setCarrera] = useState(null);
+    const [carreras, setCarreras] = useState(null);
     const [isCarreraDisabled, setIsCarreraDisabled] = useState(true);
     const [isSearchButtonDisabled, setIsSearchButtonDisabled] = useState(false);
     const history = useHistory();
+    const [render, setRender] = useState(false);
 
     const pasos = [
         "Selecciona tu universidad",
         "Selecciona tu carrera",
         "Comenza a trackear tu progreso !"
-        ];
+    ];
     
     useEffect(() => {
+        const getData = () => {
+            if (!render) {
+                axios.get('http://localhost:3001/api/universidades')
+                .then(response => {
+                    setUniversidades(response.data);
+                    setRender(true);
+                });
+            }
+        }
+        getData();
         setIsCarreraDisabled(universidad === null);
         setIsSearchButtonDisabled(carrera === null);
-    }, [universidad, carrera])
+    }, [universidad, carrera, render])
     
+    const getCarreras = (codigoUniversidad) => {
+        setUniversidad(codigoUniversidad)
+        let universidad = universidades.find(universidad => universidad.codigo === codigoUniversidad);
+        setCarreras(universidad.carreras);
+    }
+
     const pageTitle = () => {
         return <>
             <SchoolIcon className={classes.icon} />
@@ -57,28 +77,24 @@ export default function Home() {
     }
 
     function goToCarrera() {
-        history.push('/carrera');
+        history.push({
+            pathname: '/carrera',
+            state: {codigoCarrera: carrera}
+        });
     }
-
-    const universidades = [
-        'Universidad Nacional de Quilmes',
-        'Universidad de Buenos Aires'
-    ];
-    const unqCarreras = [
-        'Tecnicatura en Programación Informática',
-        'Bioinformática'
-    ]
 
     const searchBox = (searchBoxOptions, searchBoxLabel, setCallback, isDisabled = false) => {
         return <>
             <Autocomplete
                 id="disabled-options-demo"
                 onChange={(event, newValue) => {
-                    setCallback(newValue);
+                    console.log(newValue.codigo);
+                    setCallback(newValue.codigo);
                 }}
                 disabled={isDisabled}
                 options={searchBoxOptions}
-                getOptionDisabled={(option) => option === 'Universidad de Buenos Aires' || option === 'Bioinformática'}
+                getOptionLabel={(option) => option.nombre}
+                getOptionDisabled={(option) => !option.habilitado}
                 style={{ width: 300 , marginTop: 10}}
                 renderInput={(params) => (
                     <TextField {...params} label={searchBoxLabel} variant="outlined" />
@@ -111,8 +127,8 @@ export default function Home() {
                 <Grid item style={{marginTop: 15, marginBottom: 15}}>
                     <Card>
                         <CardContent>
-                            {searchBox(universidades, 'Seleccioná tu universidad', setUniversidad)}
-                            {searchBox(unqCarreras, 'Seleccioná tu carrera', setCarrera, isCarreraDisabled)}
+                            {searchBox(universidades, 'Seleccioná tu universidad', getCarreras)}
+                            {searchBox(carreras, 'Seleccioná tu carrera', setCarrera, isCarreraDisabled)}
                             <Button 
                                 variant="contained" 
                                 color="primary" 
